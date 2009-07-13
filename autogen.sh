c27a20ba47b9a2226272b5e6846170c9c453e8ca
Jürg Billeter <j@bitron.ch>
2009-07-13 14:27:06 +0200

    Check for valac >= 0.7.0 in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,16 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
+test -z "$VALAC" && VALAC=valac
+if ! $VALAC --version | sed -e 's/^.*\([0-9]\+\.[0-9]\+\)\.[0-9]\+.*$/\1/' | grep -vq '^0\.[0-6]$'
+then
+    echo "**Error**: You must have valac >= 0.7.0 installed"
+    echo "  to build vala. Download the appropriate package"
+    echo "  from your distribution or get the source tarball at"
+    echo "  http://download.gnome.org/sources/vala/"
+    exit 1
+fi
+
 # Automake requires that ChangeLog exist.
 touch ChangeLog
 mkdir -p m4
