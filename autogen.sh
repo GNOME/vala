d78f664ae249124a67e15bc4b6c4ae07e87dffc2
Jürg Billeter <j@bitron.ch>
2010-10-21 10:23:36 +0200

    build: Require valac >= 0.10.0 to build from git
    Fixes bug 631729.

--- autogen.sh
+++ autogen.sh
@@ -7,9 +7,9 @@ ORIGDIR=`pwd`
 cd $srcdir
 
 test -z "$VALAC" && VALAC=valac
-if ! $VALAC --version | sed -e 's/^.*\([0-9]\+\.[0-9]\+\)\.[0-9]\+.*$/\1/' | grep -vq '^0\.[0-6]$'
+if ! $VALAC --version | sed -e 's/^.*\([0-9]\+\.[0-9]\+\)\.[0-9]\+.*$/\1/' | grep -vq '^0\.[0-9]$'
 then
-    echo "**Error**: You must have valac >= 0.7.0 installed"
+    echo "**Error**: You must have valac >= 0.10.0 installed"
     echo "  to build vala. Download the appropriate package"
     echo "  from your distribution or get the source tarball at"
     echo "  http://download.gnome.org/sources/vala/"
