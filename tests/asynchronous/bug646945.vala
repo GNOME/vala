GNOME/vala

commit 474611603ae6df7792f4dc2f107fbafb77cc4f62

Forbid access to async callback out of the relative async method
Fixes bug 646945.

vala/valamemberaccess.vala
@@ -539,6 +539,12 @@ public class Vala.MemberAccess : Expression {
 				// and also for lambda expressions within async methods
 				var async_method = context.analyzer.current_async_method;
 
+				if (async_method == null || m != async_method.get_callback_method ()) {
+					error = true;
+					Report.error (source_reference, "Access to async callback `%s' not allowed in this context".printf (m.get_full_name ()));
+					return false;
+				}
+
 				if (async_method != context.analyzer.current_method) {
 					Symbol sym = context.analyzer.current_method;
 					while (sym != async_method) {

commit 9ee54bf41b8cf4ea29bb673e75557014a5f83d9f

Fix checking access to async callback for base methods
Fixes regression introduced by 474611603ae6df7792f4dc2f107.

vala/valamemberaccess.vala
@@ -539,7 +539,17 @@ public class Vala.MemberAccess : Expression {
 				// and also for lambda expressions within async methods
 				var async_method = context.analyzer.current_async_method;
 
-				if (async_method == null || m != async_method.get_callback_method ()) {
+				bool is_valid_access = false;
+				if (async_method != null) {
+					if (m == async_method.get_callback_method ()) {
+						is_valid_access = true;
+					} else if (async_method.base_method != null && m == async_method.base_method.get_callback_method ()) {
+						is_valid_access = true;
+					} else if (async_method.base_interface_method != null && m == async_method.base_interface_method.get_callback_method ()) {
+						is_valid_access = true;
+					}
+				}
+				if (!is_valid_access) {
 					error = true;
 					Report.error (source_reference, "Access to async callback `%s' not allowed in this context".printf (m.get_full_name ()));
 					return false;

tests/Makefile.am
@@ -95,6 +95,7 @@ TESTS = \
 	asynchronous/bug613484.vala \
 	asynchronous/bug620740.vala \
 	asynchronous/bug639591.vala \
+	asynchronous/bug646945.vala \
 	asynchronous/closures.vala \
 	dbus/basic-types.test \
 	dbus/arrays.test \

tests/asynchronous/bug646945.vala
@@ -1,0 +1,20 @@
+class Foo : Object {
+	public virtual async void method1 () { }
+}
+
+interface Bar : Object {
+	public virtual async void method2 () { }
+}
+
+class Baz : Foo, Bar {
+	public override async void method1 () {
+		method1.callback ();
+	}
+
+	public async void method2 () {
+		method2.callback ();
+	}
+}
+
+void main () {
+}