GNOME/vala

commit 0d5af7406b390f937acc1a7cff8bc926483587ff

girparser: Allow specifying the error thrown from a function in metadata

vala/valagirparser.vala
@@ -61,7 +61,8 @@ public class Vala.GirParser : CodeVisitor {
 		VIRTUAL,
 		ABSTRACT,
 		SCOPE,
-		STRUCT;
+		STRUCT,
+		THROWS;
 
 		public static ArgumentType? from_string (string name) {
 			var enum_class = (EnumClass) typeof(ArgumentType).class_ref ();
@@ -2425,8 +2426,15 @@ public class Vala.GirParser : CodeVisitor {
 			}
 		}
 
-		if (throws_string == "1") {
-			s.add_error_type (new ErrorType (null, null));
+		if (!(metadata.get_expression (ArgumentType.THROWS) is NullLiteral)) {
+			if (metadata.has_argument (ArgumentType.THROWS)) {
+				var error_types = metadata.get_string(ArgumentType.THROWS).split(",");
+				foreach (var error_type in error_types) {
+					s.add_error_type (parse_type_from_string (error_type, true, metadata.get_source_reference (ArgumentType.THROWS)));
+				}
+			} else {
+				s.add_error_type (new ErrorType (null, null));
+			}
 		}
 
 		current.symbol = s;

commit 6866309a3219f421a88887f168c0dd498b3c24e2

girparser: Fix adding error types to functions
Fixes a regression of commit 0d5af7406b390f937acc1 accidentally
missing the check on throws_string.

vala/valagirparser.vala
@@ -2432,7 +2432,7 @@ public class Vala.GirParser : CodeVisitor {
 				foreach (var error_type in error_types) {
 					s.add_error_type (parse_type_from_string (error_type, true, metadata.get_source_reference (ArgumentType.THROWS)));
 				}
-			} else {
+			} else if (throws_string == "1") {
 				s.add_error_type (new ErrorType (null, null));
 			}
 		}