GNOME/vala

commit 1e90d53dba0eff6ec2babd8292676099308dd4f6

codegen: Support owned delegate parameter in set accessor

codegen/valaccodebasemodule.vala
@@ -1343,6 +1343,9 @@ public abstract class Vala.CCodeBaseModule : CodeGenerator {
 			}
 		} else if ((acc.value_type is DelegateType) && ((DelegateType) acc.value_type).delegate_symbol.has_target) {
 			function.add_parameter (new CCodeParameter (get_delegate_target_cname (acc.readable ? "result" : "value"), acc.readable ? "gpointer*" : "gpointer"));
+			if (!acc.readable && acc.value_type.value_owned) {
+				function.add_parameter (new CCodeParameter (get_delegate_target_destroy_notify_cname ("value"), "GDestroyNotify"));
+			}
 		}
 
 		if (prop.is_private_symbol () || (!acc.readable && !acc.writable) || acc.access == SymbolAccessibility.PRIVATE) {
@@ -1439,6 +1442,9 @@ public abstract class Vala.CCodeBaseModule : CodeGenerator {
 				}
 			} else if ((acc.value_type is DelegateType) && ((DelegateType) acc.value_type).delegate_symbol.has_target) {
 				function.add_parameter (new CCodeParameter (get_delegate_target_cname (acc.readable ? "result" : "value"), acc.readable ? "gpointer*" : "gpointer"));
+				if (!acc.readable && acc.value_type.value_owned) {
+					function.add_parameter (new CCodeParameter (get_delegate_target_destroy_notify_cname ("value"), "GDestroyNotify"));
+				}
 			}
 
 			if (prop.is_private_symbol () || !(acc.readable || acc.writable) || acc.access == SymbolAccessibility.PRIVATE) {
@@ -1494,6 +1500,9 @@ public abstract class Vala.CCodeBaseModule : CodeGenerator {
 					}
 				} else if ((acc.value_type is DelegateType) && ((DelegateType) acc.value_type).delegate_symbol.has_target) {
 					vcall.add_argument (new CCodeIdentifier (get_delegate_target_cname ("value")));
+					if (!acc.readable && acc.value_type.value_owned) {
+						vcall.add_argument (new CCodeIdentifier (get_delegate_target_destroy_notify_cname ("value")));
+					}
 				}
 
 				ccode.add_expression (vcall);
@@ -1556,6 +1565,9 @@ public abstract class Vala.CCodeBaseModule : CodeGenerator {
 				}
 			} else if ((acc.value_type is DelegateType) && ((DelegateType) acc.value_type).delegate_symbol.has_target) {
 				function.add_parameter (new CCodeParameter (get_delegate_target_cname (acc.readable ? "result" : "value"), acc.readable ? "gpointer*" : "gpointer"));
+				if (!acc.readable && acc.value_type.value_owned) {
+					function.add_parameter (new CCodeParameter (get_delegate_target_destroy_notify_cname ("value"), "GDestroyNotify"));
+				}
 			}
 
 			if (!is_virtual) {
@@ -5385,6 +5397,9 @@ public abstract class Vala.CCodeBaseModule : CodeGenerator {
 			var delegate_type = (DelegateType) prop.property_type;
 			if (delegate_type.delegate_symbol.has_target) {
 				ccall.add_argument (get_delegate_target_cvalue (value));
+				if (delegate_type.value_owned) {
+					ccall.add_argument (get_delegate_target_destroy_notify_cvalue (value));
+				}
 			}
 		}
 

tests/objects/properties.vala
@@ -1,7 +1,11 @@
 using GLib;
 
+public delegate void Delegate ();
+
 public class Sample : Object {
 	private string automatic { get; set; }
+
+	public Delegate deleg { get; owned set; }
 
 	private string _name;
 	public string name {
@@ -40,6 +44,8 @@ public class Sample : Object {
 		stdout.printf("name: %s\n", name);
 		stdout.printf("read_only: %s\n", read_only);
 		stdout.printf("automatic: %s\n", automatic);
+
+		this.deleg = null;
         }
 
 	public static int main () {

commit e514de6545158e5974730cf2a95b6fa942ab3870

codegen: Fix regression when setting delegate properties
The regression was introduced by 1e90d53dba0eff6ec2b.

Fixes bug 653138.

codegen/valaccodebasemodule.vala
@@ -5394,7 +5394,7 @@ public abstract class Vala.CCodeBaseModule : CodeGenerator {
 			var delegate_type = (DelegateType) prop.property_type;
 			if (delegate_type.delegate_symbol.has_target) {
 				ccall.add_argument (get_delegate_target_cvalue (value));
-				if (delegate_type.value_owned) {
+				if (base_property.set_accessor.value_type.value_owned) {
 					ccall.add_argument (get_delegate_target_destroy_notify_cvalue (value));
 				}
 			}

tests/Makefile.am
@@ -91,6 +91,7 @@ TESTS = \
 	objects/bug628639.vala \
 	objects/bug642809.vala \
 	objects/bug643711.vala \
+	objects/bug653138.vala \
 	errors/errors.vala \
 	errors/bug567181.vala \
 	errors/bug579101.vala \

tests/objects/bug653138.vala
@@ -1,0 +1,10 @@
+public class Foo {
+	public SourceFunc foo { private set; get; }
+
+	public void bar () {
+		foo = null;
+	}
+}
+
+void main() {
+}