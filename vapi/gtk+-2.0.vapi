GNOME/vala

commit 227214f0d92b06e029961135875f25c11dfa3068

gtk+-2.0, gtk+-3.0: Mark ActionCallback delegate to not have a target
Fixes bug 653046.

vapi/gtk+-2.0.vapi
@@ -7405,7 +7405,7 @@ namespace Gtk {
 	public delegate bool AccelGroupFindFunc (Gtk.AccelKey key, GLib.Closure closure);
 	[CCode (cheader_filename = "gtk/gtk.h", has_target = false)]
 	public delegate void AccelMapForeach (void* data, string accel_path, uint accel_key, Gdk.ModifierType accel_mods, bool changed);
-	[CCode (cheader_filename = "gtk/gtk.h")]
+	[CCode (cheader_filename = "gtk/gtk.h", has_target = false)]
 	public delegate void ActionCallback (Gtk.Action action);
 	[CCode (cheader_filename = "gtk/gtk.h")]
 	public delegate int AssistantPageFunc (int current_page);

vapi/gtk+-3.0.vapi
@@ -7077,7 +7077,7 @@ namespace Gtk {
 	public delegate bool AccelGroupFindFunc (Gtk.AccelKey key, GLib.Closure closure);
 	[CCode (cheader_filename = "gtk/gtk.h", has_target = false)]
 	public delegate void AccelMapForeach (void* data, string accel_path, uint accel_key, Gdk.ModifierType accel_mods, bool changed);
-	[CCode (cheader_filename = "gtk/gtk.h")]
+	[CCode (cheader_filename = "gtk/gtk.h", has_target = false)]
 	public delegate void ActionCallback (Gtk.Action action);
 	[CCode (cheader_filename = "gtk/gtk.h")]
 	public delegate int AssistantPageFunc (int current_page);

vapi/packages/gtk+-2.0/gtk+-2.0-custom.vala
@@ -106,6 +106,7 @@ namespace Gtk {
 	[CCode (has_target = false)]
 	public delegate void CallbackMarshal (Object object, void* data, Arg[] args);
 
+	[CCode (has_target = false)]
 	public delegate void ActionCallback (Action action);
 
 	public delegate void MenuPositionFunc (Gtk.Menu menu, out int x, out int y, out bool push_in);

vapi/packages/gtk+-3.0/gtk+-3.0-custom.vala
@@ -68,6 +68,7 @@ namespace Gtk {
 		public GLib.SList<GLib.File> get_files ();
 	}
 
+	[CCode (has_target = false)]
 	public delegate void ActionCallback (Action action);
 
 	public delegate void MenuPositionFunc (Gtk.Menu menu, out int x, out int y, out bool push_in);

commit cca64f8124ac359aadaae33b2d4a6cfc06b8bae7

gtk+-2.0, gtk+-3.0: Fix ActionCallback bindings and usage in fields
Fix the regression introduced by commit 227214f0d92b06e029961.

Fixes bug 653046.

vapi/gtk+-2.0.vapi
@@ -6249,7 +6249,7 @@ namespace Gtk {
 		public weak string label;
 		public weak string accelerator;
 		public weak string tooltip;
-		[CCode (type = "GCallback")]
+		[CCode (type = "GCallback", delegate_target = false)]
 		public weak Gtk.ActionCallback callback;
 	}
 	[CCode (type_id = "GTK_TYPE_ALLOCATION", cheader_filename = "gtk/gtk.h")]
@@ -6424,7 +6424,7 @@ namespace Gtk {
 		public weak string label;
 		public weak string accelerator;
 		public weak string tooltip;
-		[CCode (type = "GCallback")]
+		[CCode (type = "GCallback", delegate_target = false)]
 		public weak Gtk.ActionCallback callback;
 		public bool is_active;
 	}
@@ -7405,7 +7405,7 @@ namespace Gtk {
 	public delegate bool AccelGroupFindFunc (Gtk.AccelKey key, GLib.Closure closure);
 	[CCode (cheader_filename = "gtk/gtk.h", has_target = false)]
 	public delegate void AccelMapForeach (void* data, string accel_path, uint accel_key, Gdk.ModifierType accel_mods, bool changed);
-	[CCode (cheader_filename = "gtk/gtk.h", has_target = false)]
+	[CCode (cheader_filename = "gtk/gtk.h")]
 	public delegate void ActionCallback (Gtk.Action action);
 	[CCode (cheader_filename = "gtk/gtk.h")]
 	public delegate int AssistantPageFunc (int current_page);

vapi/gtk+-3.0.vapi
@@ -6057,7 +6057,7 @@ namespace Gtk {
 		public weak string label;
 		public weak string accelerator;
 		public weak string tooltip;
-		[CCode (type = "GCallback")]
+		[CCode (type = "GCallback", delegate_target = false)]
 		public weak Gtk.ActionCallback callback;
 	}
 	[CCode (type_id = "GTK_TYPE_ALLOCATION", cheader_filename = "gtk/gtk.h")]
@@ -6245,7 +6245,7 @@ namespace Gtk {
 		public weak string label;
 		public weak string accelerator;
 		public weak string tooltip;
-		[CCode (type = "GCallback")]
+		[CCode (type = "GCallback", delegate_target = false)]
 		public weak Gtk.ActionCallback callback;
 		public bool is_active;
 	}
@@ -7077,7 +7077,7 @@ namespace Gtk {
 	public delegate bool AccelGroupFindFunc (Gtk.AccelKey key, GLib.Closure closure);
 	[CCode (cheader_filename = "gtk/gtk.h", has_target = false)]
 	public delegate void AccelMapForeach (void* data, string accel_path, uint accel_key, Gdk.ModifierType accel_mods, bool changed);
-	[CCode (cheader_filename = "gtk/gtk.h", has_target = false)]
+	[CCode (cheader_filename = "gtk/gtk.h")]
 	public delegate void ActionCallback (Gtk.Action action);
 	[CCode (cheader_filename = "gtk/gtk.h")]
 	public delegate int AssistantPageFunc (int current_page);

vapi/packages/gtk+-2.0/gtk+-2.0-custom.vala
@@ -106,7 +106,6 @@ namespace Gtk {
 	[CCode (has_target = false)]
 	public delegate void CallbackMarshal (Object object, void* data, Arg[] args);
 
-	[CCode (has_target = false)]
 	public delegate void ActionCallback (Action action);
 
 	public delegate void MenuPositionFunc (Gtk.Menu menu, out int x, out int y, out bool push_in);

vapi/packages/gtk+-2.0/gtk+-2.0.metadata
@@ -26,7 +26,7 @@ gtk_action_new.tooltip nullable="1"
 gtk_action_new.stock_id nullable="1"
 GtkAction::activate has_emitter="1"
 GtkActionEntry is_value_type="1"
-GtkActionEntry.callback type_name="ActionCallback" ctype="GCallback" value_owned="0"
+GtkActionEntry.callback type_name="ActionCallback" ctype="GCallback" no_delegate_target="1" value_owned="0"
 gtk_action_group_add_action_with_accel.accelerator nullable="1"
 gtk_action_group_add_actions.user_data hidden="0"
 gtk_action_group_add_actions_full.user_data hidden="0"
@@ -650,7 +650,7 @@ gtk_toggle_action_new.tooltip nullable="1"
 gtk_toggle_action_new.stock_id nullable="1"
 GtkToggleAction::toggled has_emitter="1"
 GtkToggleActionEntry is_value_type="1"
-GtkToggleActionEntry.callback type_name="ActionCallback" ctype="GCallback" value_owned="0"
+GtkToggleActionEntry.callback type_name="ActionCallback" ctype="GCallback" no_delegate_target="1" value_owned="0"
 GtkToggleButton::toggled has_emitter="1"
 GtkToolbar.GtkTooltips hidden="1"
 GtkToolbar.gpointer hidden="1"

vapi/packages/gtk+-3.0/gtk+-3.0-custom.vala
@@ -68,7 +68,6 @@ namespace Gtk {
 		public GLib.SList<GLib.File> get_files ();
 	}
 
-	[CCode (has_target = false)]
 	public delegate void ActionCallback (Action action);
 
 	public delegate void MenuPositionFunc (Gtk.Menu menu, out int x, out int y, out bool push_in);

vapi/packages/gtk+-3.0/gtk+-3.0.metadata
@@ -26,7 +26,7 @@ gtk_action_new.tooltip nullable="1"
 gtk_action_new.stock_id nullable="1"
 GtkAction::activate has_emitter="1"
 GtkActionEntry is_value_type="1"
-GtkActionEntry.callback type_name="ActionCallback" ctype="GCallback" value_owned="0"
+GtkActionEntry.callback type_name="ActionCallback" ctype="GCallback" no_delegate_target="1" value_owned="0"
 gtk_action_group_add_action_with_accel.accelerator nullable="1"
 gtk_action_group_add_actions.user_data hidden="0"
 gtk_action_group_add_actions_full.user_data hidden="0"
@@ -725,7 +725,7 @@ gtk_toggle_action_new.tooltip nullable="1"
 gtk_toggle_action_new.stock_id nullable="1"
 GtkToggleAction::toggled has_emitter="1"
 GtkToggleActionEntry is_value_type="1"
-GtkToggleActionEntry.callback type_name="ActionCallback" ctype="GCallback" value_owned="0"
+GtkToggleActionEntry.callback type_name="ActionCallback" ctype="GCallback" no_delegate_target="1" value_owned="0"
 GtkToggleButton::toggled has_emitter="1"
 GtkToolbar.GtkTooltips hidden="1"
 GtkToolbar.gpointer hidden="1"