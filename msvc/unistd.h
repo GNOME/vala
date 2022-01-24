89f72af327ba275a7a5f192f23e653d0d2a4f5ed
Chun-wei Fan <fanchunwei@src.gnome.org>
2022-01-24 19:35:40 +0800

    build: Add Visual Studio compat headers
    Visual Studio does not come with unistd.h and utime.h, so building items
    that originate from glib-2.0.vapi et al will fail if items that depend
    on these headers are used.
    
    Do overcome this, add compat headers for them that will include the
    appropriate headers from Visual Studio (or the Windows SDK), to fix the
    build.

---
+++ msvc/unistd.h
@@ -0,0 +1,10 @@
+/* Compatbility header for Visual Studio-like builds */
+
+#ifndef _MSC_VER
+# error This header is for Visual Studio-like builds only
+#endif
+
+#include <direct.h>
+#include <io.h>
+#include <process.h>
+
---
+++ msvc/utime.h
@@ -0,0 +1,7 @@
+/* compat header for MSVC builds */
+
+#ifndef _MSC_VER
+#error This compat header is for Visual Studio-like builds only
+#endif
+
+#include <sys/utime.h>
